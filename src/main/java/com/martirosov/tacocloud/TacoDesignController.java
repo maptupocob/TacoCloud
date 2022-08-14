52c83749c14e0962f8e0ca4c948c12780a97f42a
maptu <maptu@maptu.ru>
2022-08-14 17:29:10 +0300

    add test classes

--- src/main/java/com/martirosov/tacocloud/TacoDesignController.java
+++ src/main/java/com/martirosov/tacocloud/TacoDesignController.java
@@ -3,11 +3,15 @@ package com.martirosov.tacocloud;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 
 @Slf4j
 @Controller
 public class TacoDesignController {
+
+    @ModelAttribute
+
 
 
 }

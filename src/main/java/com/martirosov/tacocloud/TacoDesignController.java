2405e1a12264759754162f4fd145d7e39ffa800e
maptu <maptu@maptu.ru>
2022-08-14 23:22:39 +0300

    create OrderController

--- src/main/java/com/martirosov/tacocloud/TacoDesignController.java
+++ src/main/java/com/martirosov/tacocloud/TacoDesignController.java
@@ -4,12 +4,10 @@ package com.martirosov.tacocloud;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.martirosov.tacocloud.Ingredient.Type;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.context.request.RequestAttributes;
 
 import java.util.Arrays;
 import java.util.List;
@@ -54,6 +52,14 @@ public class TacoDesignController {
     @GetMapping
     public String design(){
         return "design";
+    }
+
+    @PostMapping
+    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder){
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+        return "redirect:/orders/current";
+
     }
 
 }
--- src/main/java/com/martirosov/tacocloud/TacoOrder.java
+++ src/main/java/com/martirosov/tacocloud/TacoOrder.java
@@ -7,10 +7,11 @@ import java.util.List;
 
 @Data
 public class TacoOrder {
-    private String id;
     private String deliveryAddress;
     private String deliveryName;
-    private String cardNumber;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
     private List<Taco> tacos = new ArrayList<>();
 
     public void addTaco(Taco taco){

ce147dc42b665f3595988c9d0e8ed9b68a98ea47
maptu <maptu@maptu.ru>
2022-10-16 20:41:49 +0300

    add Spring Data JPA

--- src/main/java/com/martirosov/tacocloud/model/TacoOrder.java
+++ src/main/java/com/martirosov/tacocloud/model/TacoOrder.java
@@ -1,24 +1,21 @@
 package com.martirosov.tacocloud.model;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.CreditCardNumber;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
-import javax.validation.constraints.Digits;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
-@Table
+@Entity
 public class TacoOrder implements Serializable {
     public static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private Date placedAt;
     @NotBlank(message = "Address is required")
@@ -27,10 +24,11 @@ public class TacoOrder implements Serializable {
     private String deliveryName;
 //    @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
-    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$", message = "Must be formatted MM/YY")
+//    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$", message = "Must be formatted MM/YY")
     private String ccExpiration;
-    @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
+//    @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
+    @OneToMany(cascade = CascadeType.ALL)
     private List<Taco> tacos = new ArrayList<>();
 
     public void addTaco(Taco taco){

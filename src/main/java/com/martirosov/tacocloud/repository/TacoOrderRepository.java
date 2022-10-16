e8a55dc9af4c864fce40b874979cd619d35129f0
maptu <maptu@maptu.ru>
2022-10-16 20:43:14 +0300

    remove unused imports

--- src/main/java/com/martirosov/tacocloud/repository/TacoOrderRepository.java
+++ src/main/java/com/martirosov/tacocloud/repository/TacoOrderRepository.java
@@ -3,7 +3,5 @@ package com.martirosov.tacocloud.repository;
 import com.martirosov.tacocloud.model.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface TacoOrderRepository extends CrudRepository<TacoOrder, Long> {
 }

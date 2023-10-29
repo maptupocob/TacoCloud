b034e8f4fe9453fc0795a09978e5f0fff1b8fe01
maptu <maptu@maptu.ru>
2023-10-29 20:51:21 +0300

    some fixes

--- src/test/java/com/martirosov/tacocloud/HomeControllerTest.java
+++ src/test/java/com/martirosov/tacocloud/HomeControllerTest.java
@@ -1,19 +1,25 @@
 package com.martirosov.tacocloud;
 
 import com.martirosov.tacocloud.controller.HomeController;
+import com.martirosov.tacocloud.repository.IngredientsRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+
 @WebMvcTest(HomeController.class)
 class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private IngredientsRepository ingredientsRepository;
 
 
     @Test
